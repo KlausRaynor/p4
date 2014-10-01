1760747f04cc330b468f8a63b98ff0ee50c4a2fb
KlausRaynor <klaus.a.holder@gmail.com>
2014-10-01 13:36:20 -0400

    subdomain test for p4

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+ 
+	<title>P2 Subdomain Test</title>
+	<meta charset='utf-8'>
+		
+</head>
+<body>
+ 	<center>
+ 	<img src="arsenal_gear.jpg">
+	<?php 
+	echo 'Hello World! You are visiting ';
+	echo $_SERVER['SERVER_NAME']; 
+	?>
+	</center>
+</body>
+</html>
